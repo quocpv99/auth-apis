25f583a0872a5db98eff931d6ea1928681de896c
anh quoc <quocphan20111999@gmail.com>
2023-11-12 21:13:56 +0700

    auth apis

---
+++ routes/api.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+Route::post('/register', [AuthController::class, 'register'])->name('register');
+Route::middleware('jwt.verify')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+    Route::post('/refresh', [AuthController::class, 'refresh'])->name('refresh');
+    Route::get('/user-profile', [AuthController::class, 'userProfile'])->name('userProfile');
+    Route::post('/change-pass', [AuthController::class, 'changePassWord'])->name('changePassWord');    
+});
+
